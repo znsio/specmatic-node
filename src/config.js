1522dfab666e214eaf8a8076d95b8004c72d76bc
Sahil Sharma <sahilaug@gmail.com>
2020-08-20 09:17:00 +0530

    simple config file
    Former-commit-id: 07b64a916c94f39413b802a6564e157664d1b1d7

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    qontractPath: './node_modules/qontract/qontract.jar',
+    qontractJarRemotePath: 'https://github.com/qontract/qontract/releases/download/0.13.1/qontract.jar'
+};
+
+module.exports = config;
