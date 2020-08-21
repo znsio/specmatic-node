4d1db1f23c869197ba8df573f0f0689f86a28a55
Sahil Sharma <sahilaug@gmail.com>
2020-08-21 12:32:32 +0530

    convert to typescript
    Former-commit-id: df3883000601305df825af3d45168162b1393f5b

--- src/bin/startStubServer.ts
+++ src/bin/startStubServer.ts
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
+import execSh from 'exec-sh';
+import path from 'path';
+import {argv} from 'yargs';
+import { qontractJarPathLocal } from '../config';
 
 const startStubServer = () => {
-  const execSh = require('exec-sh');
-  const path = require('path');
-  const { qontractJarPathLocal } = require('../config');
   const qontractJarPath = path.resolve(qontractJarPathLocal);
-
-  const {argv} = require('yargs');
   const { qontractDir, stubDir, host, port} = argv;
-
-  const qontracts = path.resolve(qontractDir);
-  const stubs = path.resolve(stubDir);
+  const qontracts = path.resolve(qontractDir + '');
+  const stubs = path.resolve(stubDir + '');
 
   console.log('starting qontract stub server')
   execSh(
     `java -jar ${qontractJarPath} stub ${qontracts} --data ${stubs} --host=${host} --port=${port}`,
     {  },
-    err => {
+    (err: any) => {
       if (err) {
         console.log('Exit code: ', err.code);
       }
