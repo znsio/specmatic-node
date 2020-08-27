956756a8372e32f1675fbb048621640f6adce6c2
Sahil Sharma <sahilaug@gmail.com>
2020-08-27 09:27:09 +0530

    basic loadStubMethod
    Former-commit-id: 010221d59d1982588c0bb838263bb402eb046339

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -10,9 +10,9 @@ var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var loadDynamicStub = () => {
-  (0, _nodeFetch.default)('http://localhost:8000/_stub_setup', {
+  (0, _nodeFetch.default)('http://localhost:8000/_qontract/expectations', {
     method: 'POST',
-    body: '{"mock-http-request": {"method": "GET","path": "/pets/1"},"mock-http-response": {"status": 200,"body": {"name": "new","type": "new","status": "new","id": "new"}}}'
+    body: '{"http-request": {"method": "GET"}, "http-response": {"status": 200,"body": {"name": "new","type": "new","status": "new","id": "new"}}}'
   }).then(res => res.json()).then(json => console.log(json));
 };
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,11 @@
 import fetch from 'node-fetch';
 
 export const loadDynamicStub = () => {
-    fetch('http://localhost:8000/_stub_setup', { method: 'POST', body: '{"mock-http-request": {"method": "GET","path": "/pets/1"},"mock-http-response": {"status": 200,"body": {"name": "new","type": "new","status": "new","id": "new"}}}' })
+    fetch('http://localhost:8000/_qontract/expectations', 
+        {
+             method: 'POST', 
+             body: '{"http-request": {"method": "GET", "path": "/pets/1"}, "http-response": {"status": 200,"body": {"name": "new","type": "new","status": "new","id": "new"}}}' 
+        })
         .then(res => res.json())
         .then(json => console.log(json));
 };
