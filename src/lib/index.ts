ddd08a429060645e8aa9caa02e3d342bba98b552
Sahil Sharma <sahilaug@gmail.com>
2020-08-27 10:21:59 +0530

    start commands for test and stub servers
    Former-commit-id: aa5bbd6764cef811bc12420973546a17cbe87c7d

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,42 @@
 import fetch from 'node-fetch';
 import path from 'path';
+import execSh from 'exec-sh';
+import { qontractJarPathLocal } from '../config';
+
+export const startStubServer = (qontractDir: string, stubDir: string, host: string, port: string) => {
+  const qontractJarPath = path.resolve(qontractJarPathLocal);
+  const qontracts = path.resolve(qontractDir + '');
+  const stubs = path.resolve(stubDir + '');
+
+  console.log(`java -jar ${qontractJarPath} stub ${qontracts} --strict --data=${stubs} --host=${host} --port=${port}`)
+
+  console.log('starting qontract stub server')
+  execSh(
+    `java -jar ${qontractJarPath} stub ${qontracts} --strict --data=${stubs} --host=${host} --port=${port}`,
+    {  },
+    (err: any) => {
+      if (err) {
+        console.log('Exit code: ', err.code);
+      }
+    }
+  );  
+}
+
+export const startTestServer = (qontractDir: string, host: string, port: string) => {
+    const qontractJarPath = path.resolve(qontractJarPathLocal);
+    const qontracts = path.resolve(qontractDir);
+    
+    console.log('running qontract tests')
+    execSh(
+      `java -jar ${qontractJarPath} test ${qontracts} --host=${host} --port=${port}`,
+      {  },
+      (err: any) => {
+        if (err) {
+          console.log('Exit code: ', err.code);
+        }
+      }
+    );  
+  }
 
 export const loadDynamicStub = (stubPath: string) => {
     const stubResponse = require(path.resolve(stubPath))
