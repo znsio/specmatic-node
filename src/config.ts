1e57582ab21e2063b28fff9f2473009b7b8fdecb
Sahil Sharma <sahilsharma@Sahils-MacBook-Pro.local>
2020-09-10 10:24:08 +0530

    bump up the version to the latest version of the jar
    Former-commit-id: 7f4fa738c4c08b267c0a7a0f4b34c32f25ef0d29

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const qontractJarPathLocal= './node_modules/qontract/qontract.jar';
-export const qontractJarPathRemote= 'https://github.com/qontract/qontract/releases/download/0.15.0/qontract.jar';
+export const qontractJarPathRemote= 'https://github.com/qontract/qontract/releases/download/0.17.0/qontract.jar';
