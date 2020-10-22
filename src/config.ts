71d482c2d50a2e51065f9392169d17cf43cce669
Sahil Sharma <sahilaug@gmail.com>
2020-10-22 18:22:32 +0530

    bump up version to 19.1
    Former-commit-id: c9eee59ac163dc2e37b5167c2a42b95fef792e08

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const qontractJarPathLocal= './node_modules/qontract/qontract.jar';
-export const qontractJarPathRemote= 'https://github.com/qontract/qontract/releases/download/0.18.0/qontract.jar';
+export const qontractJarPathRemote= 'https://github.com/qontract/qontract/releases/download/0.19.1/qontract.jar';
