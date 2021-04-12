574bec25ac9dc74492edd65294d86f86303aa6f7
Sahil Sharma <sahil4.sharma@ril.com>
2021-04-12 10:42:36 +0530

    removes dist from repo
    Former-commit-id: 014654d495367792dd4128b51a0d83c6b1daf410

--- dist/bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _core = _interopRequireDefault(require("./core"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _core.default)();
