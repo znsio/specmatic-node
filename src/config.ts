457493c4c398d52372c11ff56e077bc6829ec709
sarvesh.sharma <sarvesh.sharma@ril.com>
2021-05-06 12:11:19 +0530

    Updated path inside config.ts for specmatic.json

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const specmaticJarPathLocal = './node_modules/specmatic/specmatic.jar';
 export const specmaticJarPathRemote = 'https://github.com/znsio/specmatic/releases/download/0.24.2/specmatic.jar';
-export const specmatic = 'temp/specmatic.json'
+export const specmatic = 'specmatic.json'
