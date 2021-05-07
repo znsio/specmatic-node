8373ded76b889c6b0179ac5f8998af804785e775
sarvesh.sharma <sarvesh.sharma@ril.com>
2021-05-07 10:16:17 +0530

    Uncommented the commented test cases

--- src/lib/__tests__/index.ts
+++ src/lib/__tests__/index.ts
@@ -13,101 +13,101 @@ const stubDataPath = './data';
 const host = 'localhost';
 const port = '8000';
 
-// beforeEach(() => {
-//   execSh.mockReset();
-//   fetch.mockReset();
-// });
+beforeEach(() => {
+  execSh.mockReset();
+  fetch.mockReset();
+});
 
-// test('startStubServer method starts the specmatic stub server', () => {
-//   startStubServer(contractsPath, stubDataPath, host, port);
+test('startStubServer method starts the specmatic stub server', () => {
+  startStubServer(contractsPath, stubDataPath, host, port);
 
-//   expect(execSh).toHaveBeenCalledTimes(1);
-//   expect(execSh.mock.calls[0][0])
-//     .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} stub ${path.resolve(contractsPath)} --strict --data=${path.resolve(stubDataPath)} --host=${host} --port=${port}`);
-// });
+  expect(execSh).toHaveBeenCalledTimes(1);
+  expect(execSh.mock.calls[0][0])
+    .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} stub ${path.resolve(contractsPath)} --strict --data=${path.resolve(stubDataPath)} --host=${host} --port=${port}`);
+});
 
-// test('startTestServer runs the contract tests', () => {
-//   startTestServer(contractsPath, host, port);
+test('startTestServer runs the contract tests', () => {
+  startTestServer(contractsPath, host, port);
 
-//   expect(execSh).toHaveBeenCalledTimes(1);
-//   expect(execSh.mock.calls[0][0])
-//     .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} test ${path.resolve(contractsPath)} --host=${host} --port=${port}`);
-// });
+  expect(execSh).toHaveBeenCalledTimes(1);
+  expect(execSh.mock.calls[0][0])
+    .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} test ${path.resolve(contractsPath)} --host=${host} --port=${port}`);
+});
 
-// test('runContractTests runs the contract tests', () => {
-//   runContractTests(contractsPath, host, port);
+test('runContractTests runs the contract tests', () => {
+  runContractTests(contractsPath, host, port);
 
-//   expect(execSh).toHaveBeenCalledTimes(1);
-//   expect(execSh.mock.calls[0][0])
-//     .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} test ${path.resolve(contractsPath)} --host=${host} --port=${port}`);
-// });
+  expect(execSh).toHaveBeenCalledTimes(1);
+  expect(execSh.mock.calls[0][0])
+    .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} test ${path.resolve(contractsPath)} --host=${host} --port=${port}`);
+});
 
 
-// test('installContracts installs contracts to local', () => {
-//   installContracts();
+test('installContracts installs contracts to local', () => {
+  installContracts();
 
-//   expect(execSh).toHaveBeenCalledTimes(1);
-//   expect(execSh.mock.calls[0][0])
-//     .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} install`);
-// });
+  expect(execSh).toHaveBeenCalledTimes(1);
+  expect(execSh.mock.calls[0][0])
+    .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} install`);
+});
 
-// test('installSpecs installs contracts to local', () => {
-//   installSpecs();
+test('installSpecs installs contracts to local', () => {
+  installSpecs();
 
-//   expect(execSh).toHaveBeenCalledTimes(1);
-//   expect(execSh.mock.calls[0][0])
-//     .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} install`);
-// });
+  expect(execSh).toHaveBeenCalledTimes(1);
+  expect(execSh.mock.calls[0][0])
+    .toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} install`);
+});
 
-// test('loadDynamicStub with default baseUrl', () => {
-//   fetch.mockReturnValue(Promise.resolve("{}"));
-//   loadDynamicStub(path.resolve('./mockStub.json'));
+test('loadDynamicStub with default baseUrl', () => {
+  fetch.mockReturnValue(Promise.resolve("{}"));
+  loadDynamicStub(path.resolve('./mockStub.json'));
 
-//   expect(fetch).toHaveBeenCalledTimes(1);
-//   expect(fetch.mock.calls[0][0]).toBe('http://localhost:9000/_specmatic/expectations');
-//   expect(fetch.mock.calls[0][1]).toMatchObject({
-//     method: 'POST',
-//     body: JSON.stringify(mockStub)
-//   });
-// });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch.mock.calls[0][0]).toBe('http://localhost:9000/_specmatic/expectations');
+  expect(fetch.mock.calls[0][1]).toMatchObject({
+    method: 'POST',
+    body: JSON.stringify(mockStub)
+  });
+});
 
-// test('setExpectations with default baseUrl', () => {
-//   fetch.mockReturnValue(Promise.resolve("{}"));
-//   setExpectations(path.resolve('./mockStub.json'));
+test('setExpectations with default baseUrl', () => {
+  fetch.mockReturnValue(Promise.resolve("{}"));
+  setExpectations(path.resolve('./mockStub.json'));
 
-//   expect(fetch).toHaveBeenCalledTimes(1);
-//   expect(fetch.mock.calls[0][0]).toBe('http://localhost:9000/_specmatic/expectations');
-//   expect(fetch.mock.calls[0][1]).toMatchObject({
-//     method: 'POST',
-//     body: JSON.stringify(mockStub)
-//   });
-// });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch.mock.calls[0][0]).toBe('http://localhost:9000/_specmatic/expectations');
+  expect(fetch.mock.calls[0][1]).toMatchObject({
+    method: 'POST',
+    body: JSON.stringify(mockStub)
+  });
+});
 
-// test('loadDynamicStub with a different baseUrl for the stub server', () => {
-//   fetch.mockReturnValue(Promise.resolve("{}"));
-//   const stubServerBaseUrl = 'http://localhost:8000/'
-//   loadDynamicStub(path.resolve('./mockStub.json'), stubServerBaseUrl);
+test('loadDynamicStub with a different baseUrl for the stub server', () => {
+  fetch.mockReturnValue(Promise.resolve("{}"));
+  const stubServerBaseUrl = 'http://localhost:8000/'
+  loadDynamicStub(path.resolve('./mockStub.json'), stubServerBaseUrl);
 
-//   expect(fetch).toHaveBeenCalledTimes(1);
-//   expect(fetch.mock.calls[0][0]).toBe(`${stubServerBaseUrl}_specmatic/expectations`);
-//   expect(fetch.mock.calls[0][1]).toMatchObject({
-//     method: 'POST',
-//     body: JSON.stringify(mockStub)
-//   });
-// });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch.mock.calls[0][0]).toBe(`${stubServerBaseUrl}_specmatic/expectations`);
+  expect(fetch.mock.calls[0][1]).toMatchObject({
+    method: 'POST',
+    body: JSON.stringify(mockStub)
+  });
+});
 
-// test('setExpectations with a different baseUrl for the stub server', () => {
-//   fetch.mockReturnValue(Promise.resolve("{}"));
-//   const stubServerBaseUrl = 'http://localhost:8000/'
-//   setExpectations(path.resolve('./mockStub.json'), stubServerBaseUrl);
+test('setExpectations with a different baseUrl for the stub server', () => {
+  fetch.mockReturnValue(Promise.resolve("{}"));
+  const stubServerBaseUrl = 'http://localhost:8000/'
+  setExpectations(path.resolve('./mockStub.json'), stubServerBaseUrl);
 
-//   expect(fetch).toHaveBeenCalledTimes(1);
-//   expect(fetch.mock.calls[0][0]).toBe(`${stubServerBaseUrl}_specmatic/expectations`);
-//   expect(fetch.mock.calls[0][1]).toMatchObject({
-//     method: 'POST',
-//     body: JSON.stringify(mockStub)
-//   });
-// });
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch.mock.calls[0][0]).toBe(`${stubServerBaseUrl}_specmatic/expectations`);
+  expect(fetch.mock.calls[0][1]).toMatchObject({
+    method: 'POST',
+    body: JSON.stringify(mockStub)
+  });
+});
 
 test('setSpecmaticEnvVariable updates the environment variable value in the specmatic.json file', () => {
   const variableObj1: Record<string, string> = { "key1": "updated-value1", "key2": "updated-value2", "key3": "updated-value3", "key4": "updated-value4" }
