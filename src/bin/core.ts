ee662715f4914b61761736bc0baf1a6cdeabfd8a
Vikram Rao <contact@vikramrao.in>
2023-06-05 15:29:36 +0530

    Fixed bug where globally installed specmatic could not run due to not finding the bundled jar
    Former-commit-id: c8d52328c296d8941aa82c5fdea6094b5ef24c06

--- src/bin/core.ts
+++ src/bin/core.ts
@@ -1,10 +1,13 @@
 import execSh from 'exec-sh';
 import path from 'path';
-import { specmaticJarPathLocal } from '../config';
+import { specmaticJarPathLocal, specmaticJarName } from '../config';
 import logger from '../common/logger';
+import { exec } from 'child_process';
+import fs from 'fs';
 
-const callSpecmaticCli = (args?: string[]) => {
-    const specmaticJarPath = path.resolve(specmaticJarPathLocal);
+const callSpecmaticCli = async (args?: string[]) => {
+    const specmaticJarPath = await getSpecmaticJarPath();
+    logger.debug(`CLI: Specmatic jar path: ${specmaticJarPath}`);
     const cliArgs = (args || process.argv).slice(2).join(' ');
     logger.info(`CLI: Running with args "${cliArgs}"`);
     execSh(`java -jar ${specmaticJarPath} ${cliArgs}`, {}, (err: any) => {
@@ -17,5 +20,28 @@ const callSpecmaticCli = (args?: string[]) => {
         }
     });
 };
+
+function getSpecmaticJarPath() {
+    return new Promise<string>((resolve, _reject) => {
+        let specmaticJarPath = path.resolve(specmaticJarPathLocal);
+        resolve(specmaticJarPath);
+    }).then(specmaticJarPath => {
+        if (!fs.existsSync(specmaticJarPath)) {
+            return getGlobalSpecmaticJarPath();
+        } else {
+            return specmaticJarPath;
+        }
+    });
+}
+
+function getGlobalSpecmaticJarPath() {
+    return new Promise((resolve, _reject) => {
+        exec('npm root -g', (_err, stdout) => {
+            const npmGlobalModuleInstallPath = stdout.replace(/\n/g, '');
+            const jarPath = path.join(npmGlobalModuleInstallPath, 'specmatic', specmaticJarName);
+            resolve(jarPath);
+        });
+    });
+}
 
 export default callSpecmaticCli;
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const specmaticJarPathLocal = './node_modules/specmatic/specmatic.jar';
+export const specmaticJarName = 'specmatic.jar';
+export const specmaticJarPathLocal = `./node_modules/specmatic/${specmaticJarName}`;
 export const specmaticJarPathRemote = 'https://github.com/znsio/specmatic/releases/download/0.24.2/specmatic.jar';
-export const specmatic = 'specmatic.json'
+export const specmatic = 'specmatic.json';
