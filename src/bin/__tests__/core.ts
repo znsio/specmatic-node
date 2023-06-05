c8d52328c296d8941aa82c5fdea6094b5ef24c06
Vikram Rao <contact@vikramrao.in>
2023-06-05 15:29:36 +0530

    Fixed bug where globally installed specmatic could not run due to not finding the bundled jar

--- src/bin/__tests__/core.ts
+++ src/bin/__tests__/core.ts
@@ -1,13 +1,34 @@
 import execSh from 'exec-sh';
 import path from 'path';
 import callSpecmaticCli from '../core';
-import { specmaticJarPathLocal } from '../../config';
+import { specmaticJarName, specmaticJarPathLocal } from '../../config';
+import fs from 'fs';
+import { exec } from 'child_process';
 
 jest.mock('exec-sh');
+jest.mock('child_process');
+const execMock = exec as unknown as jest.Mock;
 
-test('arguments to be passed correctly to Specmatic lib', () => {
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
+test('pass all wrapper arguments to the jar', async () => {
+    jest.spyOn(fs, 'existsSync').mockReturnValue(true);
     const testArgs = ['node', 'index.js', 'stub', '*.specmatic', '--data', 'src/mocks', '--host', 'localhost', '--port', '8000'];
-    callSpecmaticCli(testArgs);
+    await callSpecmaticCli(testArgs);
     expect(execSh.mock.calls[0][0]).toBe(`java -jar ${path.resolve(specmaticJarPathLocal)} ${testArgs.slice(2).join(' ')}`);
+    expect(execSh).toHaveBeenCalledTimes(1);
+});
+
+test('look for jar in specmatic globally installled directory when run from global install', async () => {
+    jest.spyOn(fs, 'existsSync').mockReturnValue(false);
+    const testArgs = ['node', 'index.js', 'stub', '*.specmatic', '--data', 'src/mocks', '--host', 'localhost', '--port', '8000'];
+    const NPM_GLOBAL_PATH = '/npm/global/path';
+    setTimeout(() => {
+        execMock.mock.calls[0][1](0, NPM_GLOBAL_PATH);
+    }, 0);
+    await callSpecmaticCli(testArgs);
+    expect(execSh.mock.calls[0][0]).toBe(`java -jar ${path.resolve(NPM_GLOBAL_PATH, 'specmatic', specmaticJarName)} ${testArgs.slice(2).join(' ')}`);
     expect(execSh).toHaveBeenCalledTimes(1);
 });
