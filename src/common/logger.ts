1a588ce54120fbceb1850de8513d579a12a45386
Vikram Rao <contact@vikramrao.in>
2023-07-18 15:19:41 +0530

    Added ability to override log level set in specmatic.json for use in CI where we might want a more detailed messages

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,34 +1,49 @@
-import { createLogger, format, transports } from 'winston';
-import fs from 'fs';
+import { config, createLogger, format, transports } from 'winston'
+import fs from 'fs'
 
 const logFormat = format.printf(({ level, message, label, timestamp }) => {
-    return `[${label}] ${timestamp} ${level}: ${message}`;
-});
+    return `[${label}] ${timestamp} ${level}: ${message}`
+})
 
 function getSpecmaticConfig() {
-    const packageJsonPath = './package.json';
-    var specmaticConfig;
+    const packageJsonPath = './package.json'
+    var specmaticConfig
     if (fs.existsSync(packageJsonPath)) {
-        let packageJsonContent;
+        let packageJsonContent
         try {
-            packageJsonContent = fs.readFileSync(packageJsonPath);
+            packageJsonContent = fs.readFileSync(packageJsonPath)
         } catch (error) {
-            packageJsonContent = "{}";
+            packageJsonContent = '{}'
         }
-        const packageConfig = JSON.parse(packageJsonContent as unknown as string);
-        specmaticConfig = packageConfig.specmatic;
+        const packageConfig = JSON.parse(packageJsonContent as unknown as string)
+        specmaticConfig = packageConfig.specmatic
     }
-    return specmaticConfig || {};
+    return specmaticConfig || {}
+}
+
+function getLogLevel() {
+    let logLevel
+    if (process.env.SPECMATIC_LOG_LEVEL) {
+        logLevel = process.env.SPECMATIC_LOG_LEVEL
+    }
+    if (!logLevel || !config.syslog.levels[logLevel]) {
+        const specmaticConfig = getSpecmaticConfig()
+        logLevel = specmaticConfig.logLevel
+    }
+    if (!logLevel || !config.syslog.levels[logLevel]) {
+        logLevel = 'warn'
+    }
+    return logLevel
 }
 
 export function initLogger() {
-    const specmaticConfig = getSpecmaticConfig();
+    let logLevel = getLogLevel()
     const logger = createLogger({
-        level: specmaticConfig.logLevel || 'warn',
+        level: logLevel,
         format: format.combine(format.label({ label: 'specmatic' }), format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }), logFormat),
         transports: [new transports.Console()],
-    });
-    return logger;
+    })
+    return logger
 }
 
-export default initLogger();
+export default initLogger()
