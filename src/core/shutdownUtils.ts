9f5ee74ee42da4261d1992f176055bdc2b33a8d1
jaydeep <jaydeepk@gmail.com>
2023-09-27 10:56:10 +0530

    Fixed bug in shutting down the java jar process when firing a SIGTERM on the node child process. Since we set shell=true on the node chld process, the  SIGTERM signal sent to the child process is not propogated all the way down to the java jar process on ubuntu and windows (it does seem to propogate on mac os). Due to this, Jest doesn't terimate properly at the end as the underluging java process is still running. Hence, used tree-kill librray to ensure that the SIGTERM signal is propogated to the entire process tree.
    Former-commit-id: 270eba97838abff4d8012f9d4da6f60bd736068c

--- src/core/shutdownUtils.ts
+++ src/core/shutdownUtils.ts
@@ -2,33 +2,20 @@ import { ChildProcess } from 'child_process';
 import nodeProcess from 'node:process';
 import terminate from 'terminate/promise';
 import logger from '../common/logger'
-
-function isProcessRunning(pid: number): boolean {
-    try {
-        // Sending signal 0 doesn't kill the process, but will throw an error if the process doesn't exist
-        nodeProcess.kill(pid, 0);
-        logger.debug('Stub process is still running and needs to be terminated')
-        return true;
-    } catch (err: any) {
-        logger.debug('Stub process has shut down due to the SIGTERM signal')
-        // If process doesn't exist, ESRCH will be the error code
-        return err.code !== 'ESRCH';
-    }
-}
+const kill = require('tree-kill');
 
 export const gracefulShutdown = async function (javaProcess: ChildProcess): Promise<void> {
-
-    logger.info('Sending SIGTERM to stop stub process')
-    javaProcess.kill('SIGTERM')
-
-    return new Promise((resolve) => {
-        setTimeout(async () => {
-            if (isProcessRunning(javaProcess.pid!)) {
-                logger.debug('Terminating stub process...')
-                await terminate(javaProcess.pid!) // Assuming terminate is a known function
+    return new Promise((resolve, reject) => {
+        logger.info('Sending SIGTERM to stop stub process')
+        kill(javaProcess.pid, 'SIGTERM', (err:Error) => {
+            if(err) {
+                logger.debug('Failed to send SIGTERM to stub process tree:', err);
+                resolve();
+            } else {
+                logger.debug('SIGTERM sent successfully to stub process tree');
+                resolve();
             }
-            resolve()
-        }, 1000)
-    })
+        });
+    });
 }
 
