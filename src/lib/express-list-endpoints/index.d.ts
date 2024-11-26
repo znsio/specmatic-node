5a57862a50f61db1af581839a4b4afdb4aacef80
Hari Krishnan <harikrishnan83@gmail.com>
2024-11-26 14:39:03 +0530

    Changes to copy index.js to dist folder

--- src/lib/express-list-endpoints/index.d.ts
+++ src/lib/express-list-endpoints/index.d.ts
@@ -1,29 +1,15 @@
 export = expressListEndpoints;
-/**
- * Returns an array of strings with all the detected endpoints
- * @param {import('express').Express | import('express').Router | any} app The express/router instance to get the endpoints from
- * @returns {Endpoint[]}
- */
 declare function expressListEndpoints(app: import('express').Express | import('express').Router | any): Endpoint[];
 declare namespace expressListEndpoints {
     export { Route, Endpoint };
 }
 type Endpoint = {
-    /**
-     * Path name
-     */
     path: string;
-    /**
-     * Methods handled
-     */
     methods: string[];
-    /**
-     * Mounted middlewares
-     */
     middlewares: string[];
 };
 type Route = {
-    methods: any;
+    methods: Object;
     path: string | string[];
     stack: any[];
 };
